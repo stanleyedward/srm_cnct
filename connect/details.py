8408308ab3621fac89c32bcb418691844bee10d1
staniscluelessaf <stanleyedward9715@gmail.com>
2023-08-26 13:45:11 +0530

    fixed pathing

--- connect/details.py
+++ connect/details.py
@@ -1,10 +1,17 @@
 import pandas as pd
+from pathlib import Path
+
+path = Path.home()
+# print(str(path)+'/srm_cnct/connect/details.csv')
+
 # import numpy as np
+data = pd.read_csv(str(path)+'/srm_cnct/connect/details.csv', header = None)
+# data = pd.read_csv(r'$HOME/srm_cnct/connect/details.csv', header = None)
 
-data = pd.read_csv('connect/details.csv', header = None)
-
+# print(cwd+'/connect/details.csv')
 USERNAME = data.iloc[0,0]
 PASSWORD = data.iloc[0,1] 
+
 
 # print(USERNAME.dtype)
 
